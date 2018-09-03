ce73e0d7a270366c9c14b6b55b7c96d62fa0c222
ZhangWei <350095093@qq.com>
2018-09-03 15:23:08 +0800

    Add new files

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,Image,Button} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+
+export default class App extends Component<Props> {
+    componentDidMount(){
+        SplashScreen.hide();
+        // setTimeout(()=>{
+        //     SplashScreen.hide();
+        // },2000)   
+    }
+
+    click = () =>{
+        console.log("xx")
+    }
+    
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={{position:"absolute",zIndex:100,bottom:"30%"}}>
+        <Button
+         onPress={this.click}
+         title="进入逗音"
+         color="green"
+         accessibilityLabel="Learn more about this purple button"
+      />
+       </View>
+        <Image source={require('./img/welcome.png')}style={{width: "100%", height:"100%"}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
